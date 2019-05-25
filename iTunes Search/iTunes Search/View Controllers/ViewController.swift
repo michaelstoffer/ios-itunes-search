f57a484b1f8498235f9b7aa03a4461c59877aea4
Michael Stoffer <michaels@mdgadvertising.com>
2019-05-24 21:28:54 -0400

    Added project files and organized folders

---
+++ iTunes Search/iTunes Search/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunes Search
+//
+//  Created by Michael Stoffer on 5/24/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
